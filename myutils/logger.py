ee26b85348cc674d3834ea9d03b83ee340659037
Felix Bachmair <bachmair@phys.ethz.ch>
2013-03-01 12:24:50 +0000

    updated Logger:  - when creating/reopening logfile do not make a empty line but a line with    just a '#'

--- myutils/logger.py
+++ myutils/logger.py
@@ -106,7 +106,7 @@ class printer:
         self.logger1.addHandler(self.logFileHandler)
         self.logger1.setLevel(logging.INFO) 
         #self.f = open(path,'append')
-        x = '#--------LOG from %s ---------\n'%strftime("%a %d %b %Y at %Hh:%Mm:%Ss",localtime(self.timestamp))
+        x = '#\n#--------LOG from %s ---------\n#'%strftime("%a %d %b %Y at %Hh:%Mm:%Ss",localtime(self.timestamp))
         self.logToFile(x)
         #if self.f and self.loglevel > 0: self.f.write(x+'\n')
         #self.f.write()
